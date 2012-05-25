a988999939d616015a8fa70fc799b0d04320e1d3
Andrew Goodwin <andrewggoodwin@gmail.com>
2012-05-25 15:41:40 -0500

    Initial Commit

---
+++ GithubTest/GithubTest/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  GithubTest
+//
+//  Created by Andrew Goodwin on 5/25/12.
+//  Copyright (c) 2012 Conway Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ GithubTest/GithubTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GithubTest
+//
+//  Created by Andrew Goodwin on 5/25/12.
+//  Copyright (c) 2012 Conway Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
